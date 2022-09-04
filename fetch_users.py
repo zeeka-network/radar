fd41226c29d8dee360390b1df60e4f0194b9db24
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-09-04 11:50:17 +0430

    Save users in users.json

--- fetch_users.py
+++ fetch_users.py
@@ -1,23 +1,24 @@
 import discord
 import os
+import io,json
 
 GUILD_ID = 923604493378154496
-
 TOKEN = os.getenv("ZEEKABOT")
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
         guild = self.get_guild(GUILD_ID)
-        print(guild.members)
-        print(f"Logged on as {self.user}!")
-
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
+        users = {}
+        for m in guild.members:
+            name = m.name + '#' + m.discriminator
+            id = m.id
+            users[name.lower()] = id
+        with io.open("users.json", "w") as f:
+            json.dump(users, f, indent=3)
 
 
 intents = discord.Intents.default()
-intents.messages = True
 intents.members = True
 
 client = MyClient(intents=intents)
