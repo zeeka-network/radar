e1496bf7f9e4e6ea383310f071c21bb4178bb22b
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-09-16 19:04:37 +0430

    Discover new peers

--- discover.py
+++ discover.py
@@ -1,6 +1,6 @@
 import requests
 import json, io
-
+import random
 bootstrap = "152.228.155.120:8765"
 
 try:
@@ -11,15 +11,18 @@ except:
 
 while True:
     try:
+        bootstrap = random.choice(list(nodes))
         peers = requests.get(
             "http://{}/peers".format(bootstrap),
             headers={"X-ZEEKA-NETWORK-NAME": "debug"},
             timeout=2,
         ).json()
         for peer in peers["peers"]:
-            if peer["address"] not in nodes:
-                nodes.add(peer["address"])
+            if peer not in nodes:
+                nodes.add(peer)
         with io.open("nodes.json", "w") as f:
             json.dump(list(sorted(nodes)), f, indent=3)
     except KeyboardInterrupt:
         break
+    except Exception as e:
+        print(e)
